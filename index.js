bd6fae32589676d16262fe4c50d07a7b21fa131b
bugong <1761869346@qq.com>
2017-01-16 22:59:20 +0800

    增加resolve-name

---
+++ index.js
@@ -0,0 +1,5 @@
+let transformName = require('./src/transform-name');
+
+console.log(transformName.toBigCamelCase('thisIssssdAdds'));
+console.log(transformName.toLittleCamelCase('thisIssssdAdds'));
+console.log(transformName.toLowercase('thisIssssdAdds'));
---
+++ src/transform-name.js
@@ -0,0 +1,48 @@
+let regex = /[a-zA-Z][a-z]*/g;
+
+class TransformName {
+
+  resolve(name = '') {
+    let result = [];
+    let temp;
+
+    name = name.trim();
+
+    while (temp = regex.exec(name)) {
+      result.push(temp[0]);
+    }
+
+    return result;
+  }
+
+  toLowercase(name = '') {
+
+    return name.trim().toLocaleLowerCase();
+  }
+
+  toLittleCamelCase(name) {
+    let sections = this.resolve(name);
+    let temp = sections.map((item, index) => {
+
+      if (!item) return '';
+
+      return index > 0 ? item.replace(/^\w/, (str) => str.toUpperCase()) : item.replace(/^\w/, (str) => str.toLowerCase());
+    });
+
+    return temp.join('');
+  }
+
+  toBigCamelCase(name) {
+    let sections = this.resolve(name);
+    let temp = sections.map((item, index) => {
+
+      if (!item) return '';
+
+      return item.replace(/^\w/, (str) => str.toUpperCase());
+    });
+
+    return temp.join('');
+  }
+}
+
+module.exports = new TransformName();
