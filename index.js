ba6b1b90eccb9b06c38e8e976cf116ae50512716
yinyanlv <yanlv.yin@gmail.com>
2017-01-18 14:01:40 +0800

    修改creator

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ let path = require('path');
 let config = require('./config');
 let compiler = require('./lib/compiler');
 let creator = require('./lib/creator');
-
 let pageMap = require('./extjsConfig');
 
-fs.readFile('./temp/Viewport.js', 'utf-8', (err, data) => {
-  fs.writeFile('./destaaa/Viewport.js', compiler.compile(data), 'utf-8', (err) => {
-    console.log('ok');
-  });
-});
 
 creator.createFolder('./a/b/c/d');
