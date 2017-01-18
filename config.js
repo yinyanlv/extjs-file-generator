8df3c99625697e55b933d6b82dd51ac5fc8a2f93
yinyanlv <yanlv.yin@gmail.com>
2017-01-18 18:44:15 +0800

    完善各模块

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 module.exports = {
   namespace: 'App',
-  mode: 'override',  // {override | skip}，替换已存在的文件
-  basePath: '',
-  
+  mode: 'override',  // {'override' | 'skip'}，override时，覆盖已存在的同名文件
+  destBasePath: './dest',
+  templateMap: {
+    controller: './temp/Viewport.js',
+    viewport: './temp/Viewport.js',
+    query: './temp/Viewport.js',
+    grid: './temp/Viewport.js',
+    edit: './temp/Viewport.js',
+    model: './temp/Viewport.js',
+    store: './temp/Viewport.js'
+  }
 };
--- index.js
+++ index.js
@@ -4,7 +4,19 @@ let path = require('path');
 let config = require('./config');
 let compiler = require('./lib/compiler');
 let creator = require('./lib/creator');
+let utils = require('./lib/utils');
 let pageMap = require('./extjsConfig');
 
+let moduleNames = utils.getNeedCreateModuleList(pageMap);
+let totalCount = moduleNames.length;
+let curCount = 0;
 
-creator.createFolder('./a/b/c/d');
+for (let name of moduleNames) {
+  creator.writeFileByModuleName(name, 'var a = 2;', function () {
+
+    curCount++;
+    console.log('*** INFO: ' + name + ' has created! ***  ---' + (curCount / totalCount * 100).toFixed(2) + '%---');
+
+    if (curCount === totalCount) console.log('\ncompleted!!!');
+  });
+}
