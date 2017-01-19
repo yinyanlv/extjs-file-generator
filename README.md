703d7064775c1c01f63ada892bd37af670390553
yinyanlv <yanlv.yin@gmail.com>
2017-01-19 12:12:17 +0800

    初步完成

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## extjs file generator
+- node >= 6.9.*
--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   namespace: 'App',
-  mode: 'override',  // {'override' | 'skip'}，override时，覆盖已存在的同名文件
   destBasePath: './dest',
   templatePathList: [
     './test/view/Viewport.js',
